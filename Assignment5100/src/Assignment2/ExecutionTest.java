d6bc767c55cc10efffcb2a98f32ed148cdd250ed
Jian Hou <Jian Hou@192.168.2.46>
2015-10-06 18:52:50 -0700

    Final version of method test for assignment2

--- Assignment5100/src/Assignment2/ExecutionTest.java
+++ Assignment5100/src/Assignment2/ExecutionTest.java
@@ -1,8 +1,9 @@
 package Assignment2;
 
 import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
+
 
 public class ExecutionTest {
 
@@ -11,19 +12,25 @@ public class ExecutionTest {
 	@Test
 	public void testDetectEvenNumber() {
 		fail("Not yet implemented"); // TODO
-		int[] testnumbers = execution.detectEvenNumber(100);
-		
-		System.out.print(testnumbers[0]);
-		for(int i = 1; i < testnumbers.length; i++)
-			System.out.print(", " + testnumbers[i]);
+		int[] testnumbers = execution.detectEvenNumber(10);
+		Assert.assertArrayEquals(testnumbers, new int[]{2, 4, 6, 8, 10});
+		testnumbers = execution.detectEvenNumber(2);
+		Assert.assertArrayEquals(testnumbers, new int[]{2});
+		testnumbers = execution.detectEvenNumber(0);
+		Assert.assertArrayEquals(testnumbers, new int[]{});
+		testnumbers = execution.detectEvenNumber(1);
+		Assert.assertArrayEquals(testnumbers, new int[]{});
 	}
 
 	@Test
 	public void testIsPrime() {
 		fail("Not yet implemented"); // TODO
 		boolean testnum = execution.isPrime(1);
-		
-		System.out.print(testnum);
+		Assert.assertTrue(testnum);
+		testnum = execution.isPrime(97);
+		Assert.assertTrue(testnum);
+		testnum = execution.isPrime(13);
+		Assert.assertTrue(testnum);
 		
 	}
 
@@ -31,36 +38,48 @@ public class ExecutionTest {
 	public void testCountDigit() {
 		fail("Not yet implemented"); // TODO
 		int testresult = execution.countDigit(1);
-		
-		System.out.print(testresult);
+		Assert.assertEquals(testresult, 1);
+		testresult = execution.countDigit(0);
+		Assert.assertEquals(testresult, 1);
+		testresult = execution.countDigit(1433524352);
+		Assert.assertEquals(testresult, 10);
 	}
 
 	@Test
 	public void testCountSubLetterString() {
 		fail("Not yet implemented"); // TODO
-		String test = "asdfasdf";
-		int testresult = execution.countSubLetter(test);
-		
-		System.out.print("it has " + testresult + " a");
+		int testresult = execution.countSubLetter("asdfasdftest");
+		Assert.assertEquals(testresult, 2);
+		testresult = execution.countSubLetter("a");
+		Assert.assertEquals(testresult, 1);
+		testresult = execution.countSubLetter("aaaaaaaaaaaaaaaa");
+		Assert.assertEquals(testresult, 16);
+		testresult = execution.countSubLetter("");
+		Assert.assertEquals(testresult, 0);
 	}
 
 	@Test
 	public void testCountSubLetterStringChar() {
 		fail("Not yet implemented"); // TODO
-		String test = "000f000";
-		char testchar = 'f';
-		int testresult = execution.countSubLetter(test, testchar);
+		int testresult = execution.countSubLetter("000f000test", 'f');
+		Assert.assertEquals(testresult, 1);
+		testresult = execution.countSubLetter("fffffffffffffff", 'f');
+		Assert.assertEquals(testresult, 15);
+		testresult = execution.countSubLetter("t", 'f');
+		Assert.assertEquals(testresult, 0);
 		
-		System.out.print(test + " has " + testresult + " " + testchar);
 	}
 
 	@Test
 	public void testFindMaxMinNum() {
 		fail("Not yet implemented"); // TODO
-	
 		int testresult = execution.findMaxMinNum(new int[]{0,1,0,0,1}, true);
+		Assert.assertEquals(testresult, 1);
+		testresult = execution.findMaxMinNum(new int[]{1000,0,-2}, false);
+		Assert.assertEquals(testresult, -2);
+		testresult = execution.findMaxMinNum(new int[]{1000,0,-2}, true);
+		Assert.assertEquals(testresult, 1000);
 		
-		System.out.print(testresult);
 	}
 
 }
